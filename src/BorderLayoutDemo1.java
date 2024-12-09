9b10b7be57afb48c949f57d7981a736193e6ccc6
Simon-kai <1241688584@qq.com>
2024-12-09 13:33:14 +0800

    文件二次提交

--- src/BorderLayoutDemo1.java
+++ src/BorderLayoutDemo1.java
@@ -8,10 +8,11 @@ public class BorderLayoutDemo1 {
         SwingUtilities.invokeLater(() -> {
             JFrame jFrame = new JFrame("这里测试BorderLayout");
 
-            //1.给frame设置BorderLayout布局管理器
+            // 1.给frame设置BorderLayout布局管理器
             jFrame.setLayout(new BorderLayout(30, 10));
 
-            //2.像JFrame指定区域添加组件
+            // 2.像JFrame指定区域添加组件
+            // 如果不往某个区域放入组件，那么该区域就不会空白出来，而是会被其他区域占用
             jFrame.add(new Button("北侧按钮"), BorderLayout.NORTH);
             jFrame.add(new Button("南侧按钮"), BorderLayout.SOUTH);
             jFrame.add(new Button("东侧按钮"), BorderLayout.EAST);
